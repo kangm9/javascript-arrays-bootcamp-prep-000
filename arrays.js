85649510928323c145a5f377b8881acfb9ee8b96
Min Kang <kangm914@gmail.com>
2019-04-24 17:11:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.unshift();
+  return array.shift();
 }
 
 function removeElementFromBeginningOfArray(array) {
